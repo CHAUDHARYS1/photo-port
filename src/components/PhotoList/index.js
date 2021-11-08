c7750170eb8e6c7bd9f217b49e9a79f1231b4558
Shital Chaudhary <shitalchau10@gmail.com>
2021-11-08 08:37:10 -0600

    manage the modal's state

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -102,12 +104,15 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
-  const toggleModal = (image,i) =>{
-    
-  }
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div>
-      <Modal></Modal>
+      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
